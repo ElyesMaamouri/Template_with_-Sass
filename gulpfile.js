041698d70eba89539b1cd2179e3b635b332ba40a
ElyesMaamouri <elyesmaamouri@gmail.com>
2021-12-04 18:28:53 +0100

    Add nested code css

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 // Build style scss to css
 function buildStyles() {
-  return src("*.scss").pipe(sass()).pipe(dest("css"));
+  return src("shinobi/**/*.scss ").pipe(sass()).pipe(dest("css"));
 }
 // Watch any changes in file scss
 function watchTask() {
-  watch(["*.scss"], buildStyles);
+  watch(["shinobi/**/*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
